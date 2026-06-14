f40cbd8a09722458a8283aee28c18ddffa475fa5
Greendeer30 <markdtgreen@gmail.com>
2026-06-13 22:23:04 -0400

    profile fix

--- js/profile.js
+++ js/profile.js
@@ -11,8 +11,55 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 let selectedAvatar = "1.png";
 
 const avatarGrid =
-document.getElementById(
-    "avatarGrid"
+document.getElementById("avatarGrid");
+
+for(let i = 1; i <= 10; i++){
+
+    const img =
+    document.createElement("img");
+
+    img.src =
+    `avatars/${i}.png`;
+
+    img.classList.add(
+        "avatar-option"
+    );
+
+    img.onclick = () => {
+
+        selectedAvatar =
+        `${i}.png`;
+
+        document
+        .getElementById(
+            "currentAvatar"
+        )
+        .src =
+        img.src;
+    };
+
+    avatarGrid.appendChild(img);
+}
+
+await updateDoc(
+    doc(
+        db,
+        "users",
+        auth.currentUser.uid
+    ),
+    {
+        profileName:
+            profileName.value,
+
+        status:
+            status.value,
+
+        avatar:
+            selectedAvatar,
+
+        lastUpdated:
+            serverTimestamp()
+    }
 );
 
 function buildAvatarPicker(){
@@ -171,54 +218,3 @@ document
     }
 );
 
-const avatarGrid =
-document.getElementById("avatarGrid");
-
-for(let i = 1; i <= 10; i++){
-
-    const img =
-    document.createElement("img");
-
-    img.src =
-    `avatars/${i}.png`;
-
-    img.classList.add(
-        "avatar-option"
-    );
-
-    img.onclick = () => {
-
-        selectedAvatar =
-        `${i}.png`;
-
-        document
-        .getElementById(
-            "currentAvatar"
-        )
-        .src =
-        img.src;
-    };
-
-    avatarGrid.appendChild(img);
-}
-
-await updateDoc(
-    doc(
-        db,
-        "users",
-        auth.currentUser.uid
-    ),
-    {
-        profileName:
-            profileName.value,
-
-        status:
-            status.value,
-
-        avatar:
-            selectedAvatar,
-
-        lastUpdated:
-            serverTimestamp()
-    }
-);
