8d67208b1e2b261fde2ad65680986187b773319c
Sebastian Pio Roda <sebastian.pioroda@duke.edu>
2026-06-13 22:05:20 -0400

    updated location data

--- js/locations.js
+++ js/locations.js
@@ -12,8 +12,8 @@ export const locations = [
     name: "Brodhead Center",
     aliases: ["WU", "West Union"],
     category: "dining",
-    lat: 36.0009,
-    lng: -78.9397
+    lat: 36.000692, 
+    lng: -78.939441
   },
   {
     id: "bryan-center",
@@ -28,111 +28,135 @@ export const locations = [
     name: "Bus Stop",
     aliases: ["West Campus Bus Stop", "Chapel Bus Stop"],
     category: "transportation",
-    lat: 36.0012,
-    lng: -78.9404
+    lat: 36.000934, 
+    lng: -78.938278
+  },
+  {
+    id: "east-campus-bus-stop",
+    name: "Bus Stop",
+    aliases: ["East Campus Bus Stop", "Ryan's Daily Commute"],
+    category: "transportation",
+    lat: 36.005983, 
+    lng: -78.914763
   },
   {
     id: "gross-hall",
     name: "Gross Hall",
     aliases: ["Gross Hall for Interdisciplinary Innovation"],
     category: "academic",
-    lat: 36.0018,
-    lng: -78.9443
+    lat: 36.001112, 
+    lng: -78.944347
   },
   {
     id: "french-science-building",
     name: "French Science Building",
     aliases: ["French Family Science Center", "FFSC"],
     category: "academic",
-    lat: 36.0013,
-    lng: -78.9433
+    lat: 36.002896, 
+    lng: -78.943123
   },
   {
     id: "physics-building",
     name: "Physics Building",
     aliases: ["Physics"],
     category: "academic",
-    lat: 36.0026,
-    lng: -78.9434
+    lat: 36.003152, 
+    lng: -78.942160
   },
   {
     id: "lsrc",
     name: "LSRC",
     aliases: ["Levine Science Research Center"],
     category: "academic",
-    lat: 36.0018,
-    lng: -78.9463
+    lat: 36.004190, 
+    lng: -78.942084
   },
   {
     id: "hudson-hall",
     name: "Hudson Hall",
     aliases: ["Hudson", "Hudson Hall Annex"],
     category: "academic",
-    lat: 36.0031,
-    lng: -78.9407
+    lat: 36.00147,
+    lng: -78.56255
   },
   {
     id: "fitzpatrick-building",
     name: "Fitzpatrick Building",
     aliases: ["FCIEMAS", "Fitzpatrick Center"],
     category: "academic",
-    lat: 36.0036,
-    lng: -78.9397
+    lat: 36.003070, 
+    lng: -78.940242
   },
   {
     id: "wilkinson-building",
     name: "Wilkinson Building",
     aliases: ["Wilkinson"],
     category: "academic",
-    lat: 36.0040,
-    lng: -78.9388
+    lat: 36.003583, 
+    lng: -78.938316
   },
   {
-    id: "wannamaker-quad",
-    name: "Wannamaker Quad",
+    id: "wannamaker-quad(1)",
+    name: "Wannamaker Quad (Common Room 1)",
     aliases: ["Wannamaker"],
     category: "residential",
-    lat: 36.0002,
-    lng: -78.9425
+    lat: 35.998339, 
+    lng: -78.939386
   },
   {
-    id: "edens-quad",
-    name: "Edens Quad",
+    id: "wannamaker-quad(2)",
+    name: "Wannamaker Quad (Common Room 2)",
+    aliases: ["Wannamaker"],
+    category: "residential",
+    lat: 35.998951, 
+    lng: -78.938948
+  },
+  {
+    id: "edens-quad(Fishbowl)",
+    name: "Edens Quad (Fishbowl)",
     aliases: ["Edens"],
     category: "residential",
-    lat: 36.0009,
-    lng: -78.9474
+    lat: 35.998901, 
+    lng: -78.936682
+  },
+  {
+    id: "edens-quad(2A)",
+    name: "Edens Quad (2A Common Room)",
+    aliases: ["Edens"],
+    category: "residential",
+    lat: 35.998856,
+    lng: -78.935924
   },
   {
     id: "west-house-dorm",
-    name: "West House Dorm",
+    name: "West House Dorm (Ryan's Mancave)",
     aliases: ["West House"],
     category: "residential",
-    lat: 36.0010,
-    lng: -78.9417
+    lat: 36.005758, 
+    lng: -78.915117
   },
   {
   id: "wilson-gym",
   name: "Wilson Recreation Center",
   aliases: ["Wilson Gym", "Wilson"],
   category: "gym",
-  lat: 36.0063,
-  lng: -78.9396
+  lat: 35.59484,
+  lng: -78.94033
 },
 {
   id: "pascal-field-house",
   name: "Pascal Field House",
   aliases: ["Pascal"],
   category: "athletics",
-  lat: 36.0070,
-  lng: -78.9404
+  lat: 35.993680, 
+  lng: -78.942549
 },
 {
   id: "morris-williams-track",
   name: "Morris Williams Track & Field Stadium",
   aliases: ["Track", "Track & Field", "Morris Williams"],
   category: "athletics",
-  lat: 36.0077,
-  lng: -78.9411
+  lat: 35.996033, 
+  lng: -78.945246
 }
 ];
