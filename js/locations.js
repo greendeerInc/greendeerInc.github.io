9d44200ddb2bbb6f624f2e4228f76095e2cf7167
Sebastian Pio Roda <sebastian.pioroda@duke.edu>
2026-06-13 21:36:42 -0400

    created map

---
+++ js/locations.js
@@ -0,0 +1,138 @@
+export const locations = [
+  {
+    id: "perkins-library",
+    name: "Perkins Library",
+    aliases: ["Perkins"],
+    category: "library",
+    lat: 36.0022634,
+    lng: -78.9386143
+  },
+  {
+    id: "brodhead-center",
+    name: "Brodhead Center",
+    aliases: ["WU", "West Union"],
+    category: "dining",
+    lat: 36.0009,
+    lng: -78.9397
+  },
+  {
+    id: "bryan-center",
+    name: "Bryan Center",
+    aliases: ["BC", "Bryan University Center"],
+    category: "student_center",
+    lat: 36.0010,
+    lng: -78.9409
+  },
+  {
+    id: "west-campus-bus-stop",
+    name: "Bus Stop",
+    aliases: ["West Campus Bus Stop", "Chapel Bus Stop"],
+    category: "transportation",
+    lat: 36.0012,
+    lng: -78.9404
+  },
+  {
+    id: "gross-hall",
+    name: "Gross Hall",
+    aliases: ["Gross Hall for Interdisciplinary Innovation"],
+    category: "academic",
+    lat: 36.0018,
+    lng: -78.9443
+  },
+  {
+    id: "french-science-building",
+    name: "French Science Building",
+    aliases: ["French Family Science Center", "FFSC"],
+    category: "academic",
+    lat: 36.0013,
+    lng: -78.9433
+  },
+  {
+    id: "physics-building",
+    name: "Physics Building",
+    aliases: ["Physics"],
+    category: "academic",
+    lat: 36.0026,
+    lng: -78.9434
+  },
+  {
+    id: "lsrc",
+    name: "LSRC",
+    aliases: ["Levine Science Research Center"],
+    category: "academic",
+    lat: 36.0018,
+    lng: -78.9463
+  },
+  {
+    id: "hudson-hall",
+    name: "Hudson Hall",
+    aliases: ["Hudson", "Hudson Hall Annex"],
+    category: "academic",
+    lat: 36.0031,
+    lng: -78.9407
+  },
+  {
+    id: "fitzpatrick-building",
+    name: "Fitzpatrick Building",
+    aliases: ["FCIEMAS", "Fitzpatrick Center"],
+    category: "academic",
+    lat: 36.0036,
+    lng: -78.9397
+  },
+  {
+    id: "wilkinson-building",
+    name: "Wilkinson Building",
+    aliases: ["Wilkinson"],
+    category: "academic",
+    lat: 36.0040,
+    lng: -78.9388
+  },
+  {
+    id: "wannamaker-quad",
+    name: "Wannamaker Quad",
+    aliases: ["Wannamaker"],
+    category: "residential",
+    lat: 36.0002,
+    lng: -78.9425
+  },
+  {
+    id: "edens-quad",
+    name: "Edens Quad",
+    aliases: ["Edens"],
+    category: "residential",
+    lat: 36.0009,
+    lng: -78.9474
+  },
+  {
+    id: "west-house-dorm",
+    name: "West House Dorm",
+    aliases: ["West House"],
+    category: "residential",
+    lat: 36.0010,
+    lng: -78.9417
+  },
+  {
+  id: "wilson-gym",
+  name: "Wilson Recreation Center",
+  aliases: ["Wilson Gym", "Wilson"],
+  category: "gym",
+  lat: 36.0063,
+  lng: -78.9396
+},
+{
+  id: "pascal-field-house",
+  name: "Pascal Field House",
+  aliases: ["Pascal"],
+  category: "athletics",
+  lat: 36.0070,
+  lng: -78.9404
+},
+{
+  id: "morris-williams-track",
+  name: "Morris Williams Track & Field Stadium",
+  aliases: ["Track", "Track & Field", "Morris Williams"],
+  category: "athletics",
+  lat: 36.0077,
+  lng: -78.9411
+}
+];
